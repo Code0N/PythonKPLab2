44a7ebb8803ba0c11fde0c403531dc2c1fadcbd6
Code0N <redeemer.of.souls@yandex.ru>
2017-03-15 20:09:00 +0300

    Задание первое недоделанное

--- 01.py
+++ 01.py
@@ -1,1 +1,18 @@
-#С…РµР»Р»Рѕ, РІРѕСЂР»Рґ :D
+from sys import argv
+from os.path import exists
+
+if len(argv) == 1:
+	print('Укажите файл для обработки')
+	exit()
+if exists(argv[1]) == False:
+	print('Файл не существует')
+	exit()
+	
+file = open(argv[1] 'rt')
+alltextfiltered = ''
+tuplealphabet = ()
+for line in file:
+	for i in line:
+		if i.isalpha():
+			alltextfiltered += i #Because Duke say's Fuck you optimization
+
