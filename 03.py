d8ebc7dfe21da413fe1ff8a53d5f536442294e13
Code0N <redeemer.of.souls@yandex.ru>
2017-03-19 15:26:21 +0300

    Третье пофикшенное

--- 03.py
+++ 03.py
@@ -11,13 +11,22 @@ if not os.path.isdir(argv[1]):
 	print('Папка не является папкой')
 	exit()
 
-nameslist = open(os.path.join(argv[1], 'list.txt'), 'rt')
+file = open(os.path.join(argv[1], 'list.txt'), 'rt')
+nameslist = file.read().splitlines()
 
 listdir = os.listdir(argv[1])
 
 for i in listdir:
 	for j in nameslist:
 	#print(j[4:len(j)-8])
-		if i[:len(i)-4] == j[4:len(j)-8]:
-			print('Файл {} переименовывается в {}.mp3'.format(i, j[:len(j)-8]))
-			os.rename(os.path.join(argv[1], i), os.path.join(argv[1], j[:len(j)-8])+'.mp3')
+		if i[:len(i)-4] == j[4:len(j)-7]:
+			print('Файл {} переименовывается в {}.mp3'.format(i, j[:len(j)-7]))
+			os.rename(os.path.join(argv[1], i), os.path.join(argv[1], j[:len(j)-7])+'.mp3')
+
+#for i in listdir:
+#	print(i[:len(i)-4])
+#
+#print('\n')
+#
+#for i in nameslist:
+#	print(i[4:len(i)-7])
