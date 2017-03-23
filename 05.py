17c3dd0f074de018aa200c68f08d28d3b163f86a
Code0N <redeemer.of.souls@yandex.ru>
2017-03-23 19:23:18 +0300

    Задание 5 дополненное

--- 05.py
+++ 05.py
@@ -3,4 +3,4 @@ import re
 userInput = input('Введите свою бесполезную строку\n').split(' ')
 
 for i in range(len(userInput)):
-	print(re.findall('[A-Z].*\d{2,4}', userInput[i]))
+	print(re.findall('[A-ZА-Я].*\d{2,4}', userInput[i]))
