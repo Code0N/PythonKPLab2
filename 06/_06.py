2d801ea2cc1d64153b46da8f3d8c802c35e1c70a
Code0N <redeemer.of.souls@yandex.ru>
2017-03-26 10:55:09 +0300

    Шестое рабочее

--- 06/_06.py
+++ 06/_06.py
@@ -6,23 +6,34 @@ import argparse
 
 def check_arguments(args=None):
     parser = argparse.ArgumentParser(description="Reorganizer")
-    parser.add_argument('-D', '--days', help='Last modification date of file in formad dd.mm.yyyy', default='01.01.1997')
-    parser.add_argument('-P', '--path', help='Path to folder', required='True')
-    parser.add_argument('-S', '--size', help='Size of files in MB', default='10')
+    parser.add_argument('-D', '--days', help='How much older files should be', default='01.01.1997')
+    parser.add_argument('-S', '--source', help='Path to folder', required='True')
+    parser.add_argument('-R', '--size', help='Size of files in KB', default='10')
     parsed = parser.parse_args(args)
-    return parsed.days, parsed.path, parsed.size
-
-def processFile(file):
-    tempdt = datetime.fromtimestamp(os.path.getmtime(file))
-    if tempdt < standardDateTime:
-        if not os.path.exists('Archive'):
-            os.mkdir('Archive')
-            shutil.move(file, '.\\Archive')
+    return parsed.days, parsed.source, parsed.size
 
 
 date, path, size = check_arguments(argv[1:])
-standardDateTime = datetime.strptime(date, '%d.%m.%Y')
+size = int(size) * 1024
+nowDateTime = datetime.today()
+if not os.path.exists(path):
+    print('Source not exists')
+    exit()
+if not os.path.isdir(path):
+    print('Path isn\'t directory')
+    exit()
 os.chdir(path)
 listOfFiles = os.listdir()
 for i in listOfFiles:
-    processFile(i)
+    tempdt = datetime.fromtimestamp(os.path.getmtime(i))
+    difference_days = (nowDateTime - tempdt).days
+    if difference_days > int(date):
+        if not os.path.exists('.\\Archive'):
+            os.mkdir('.\\Archive')
+        shutil.move(i, '.\\Archive\\')
+        continue
+    if os.path.getsize(i) < size:
+        if not os.path.exists('.\\Small'):
+            os.mkdir('.\\Small')
+        shutil.move(i, '.\\Small\\')
+        continue
